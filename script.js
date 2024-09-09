02ed11023b18bd94886e01f5a178b917a10753cd
Mayur Dhwaj <mayurdhwaj1315@gmail.com>
2024-09-09 18:01:24 +0530

    responsive donw

--- script.js
+++ script.js
@@ -1,49 +1,50 @@
 /*=============== SHOW MENU ===============*/
-// Constantes des éléments du menu de navigation. 
-const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById('nav-close')
+// Constantes des éléments du menu de navigation.
+const navMenu = document.getElementById("nav-menu"),
+  navToggle = document.getElementById("nav-toggle"),
+  navClose = document.getElementById("nav-close");
 
 /*===== MENU SHOW =====*/
 // Valide l'existence de la constante
-if(navToggle){
-    // Ajoute la classe 'show-menu' à 'navMenu' lorsqu'on clique sur 'navToggle'.
-    navToggle.addEventListener('click', () =>{
-        navMenu.classList.add('show-menu')
-    })
+if (navToggle) {
+  // Ajoute la classe 'show-menu' à 'navMenu' lorsqu'on clique sur 'navToggle'.
+  navToggle.addEventListener("click", () => {
+    navMenu.classList.add("show-menu");
+  });
 }
 
 /*===== MENU HIDDEN =====*/
 // Valide l'existence de la constante
-if(navClose){
-    // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur 'navClose'.
-    navClose.addEventListener('click', () =>{
-        navMenu.classList.remove('show-menu')
-    })
+if (navClose) {
+  // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur 'navClose'.
+  navClose.addEventListener("click", () => {
+    navMenu.classList.remove("show-menu");
+  });
 }
 
 /*=============== REMOVE MENU MOBILE ===============*/
 // Constantes qui récupèrent tous les éléments de la classe 'nav__link'.
-const navLink = document.querySelectorAll('.nav__link')
+const navLink = document.querySelectorAll(".nav__link");
 
-const linkAction = () =>{
-    const navMenu = document.getElementById('nav-menu')
-    // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur un lien de navigation.
-    navMenu.classList.remove('show-menu')
-}
-navLink.forEach(n => n.addEventListener('click', linkAction))
+const linkAction = () => {
+  const navMenu = document.getElementById("nav-menu");
+  // Supprime la classe 'show-menu' de 'navMenu' lorsqu'on clique sur un lien de navigation.
+  navMenu.classList.remove("show-menu");
+};
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*=============== CHANGE SHADOW HEADER ===============*/
 const shadowHeader = () => {
-    const header = document.getElementById('header')
-    // Ajoute ou supprime la classe 'shadow-header' à 'header' en fonction du défilement de la page.
-    this.scrollY >= 50 ? header.classList.add('shadow-header') 
-                       : header.classList.remove('shadow-header')
-}
-window.addEventListener('scroll', shadowHeader)
+  const header = document.getElementById("header");
+  // Ajoute ou supprime la classe 'shadow-header' à 'header' en fonction du défilement de la page.
+  this.scrollY >= 50
+    ? header.classList.add("shadow-header")
+    : header.classList.remove("shadow-header");
+};
+window.addEventListener("scroll", shadowHeader);
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
-const sections = document.querySelectorAll('section[id]')
+const sections = document.querySelectorAll("section[id]");
 
 /** FONCTIONNEMENT SCROLL SECTIONS ACTIVE LINK
  * Ajoute ou supprime la classe 'active-link' aux éléments de la barre de navigation en fonction de la position de défilement de la fenêtre.
@@ -51,20 +52,31 @@ const sections = document.querySelectorAll('section[id]')
  * la classe 'active-link' est ajoutée à l'élément de navigation correspondant.
  * Sinon, la classe 'active-link' est supprimée.
  */
-const scrollActive = () =>{
-    const scrollDown = window.scrollY
+const scrollActive = () => {
+  const scrollDown = window.scrollY;
 
-	sections.forEach(current =>{
-		const sectionHeight = current.offsetHeight,
-			  sectionTop = current.offsetTop - 58,
-			  sectionId = current.getAttribute('id'),
-			  sectionsClass = document.querySelector('.nav__menu a[href*=' + sectionId + ']')
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight,
+      sectionTop = current.offsetTop - 58,
+      sectionId = current.getAttribute("id"),
+      sectionsClass = document.querySelector(
+        ".nav__menu a[href*=" + sectionId + "]"
+      );
 
-		if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
-			sectionsClass.classList.add('active-link')
-		}else{
-			sectionsClass.classList.remove('active-link')
-		}                                                    
-	})
+    if (scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight) {
+      sectionsClass.classList.add("active-link");
+    } else {
+      sectionsClass.classList.remove("active-link");
+    }
+  });
+};
+window.addEventListener("scroll", scrollActive);
+
+// download pdf
+function resume() {
+  // Specify the PDF file URL
+  const pdfUrl = "Resume.pdf"; // Replace with the actual URL to your PDF
+
+  // Open the PDF in a new tab
+  window.open(pdfUrl, "_blank");
 }
-window.addEventListener('scroll', scrollActive)
